70179ad4b5ce50a65e7dd79690644d49a5995020
AlexKaravaev <AlexKaravev@gmail.com>
2018-07-02 10:39:21 +0300

    code for video added

--- sem1/src/lab4/code/L4_for_video.py
+++ sem1/src/lab4/code/L4_for_video.py
@@ -16,49 +16,37 @@ us2.mode = 'US-DIST-CM'
 dist_wanted = 40
 
 # dist between sensors
-H = 23
+H = 19.0
 
 # koeficients
-K_p = 0.5
-K_i = 0.05
-K_d = 1.0
+K_p = 8.0
 
 fwd_V = -70
 
 def main():
+
     f = open('data.txt', 'w')
 
+    start_time = time.time()
     # using try finally to close file even if exc will be raised
     try:
-        # last integral err
-        e_i = 0
-        last_time = start_time = time.time()
-        # value is returned = mm, convert it to cm
-        d1 = us1.value() / 10
-        d2 = us1.value() / 10
-        distance = (d1 + d2) * H * math.sqrt(1.0 / (H * H + (d2 - d1) * (d2 - d1))) / 2.0
-        last_e = dist_wanted - distance
-
         while True:
             d1 = us1.value() / 10
             d2 = us2.value() / 10
 
-            dt = (time.time() - last_time) / 1000
-            last_time = time.time()
+            d1 = d1_last if d1 == 255 else d1
+            d2 = d2_last if d2 == 255 else d2
+
+            d1_last = d1
+            d2_last = d2
 
             distance = (d1 + d2) * H * math.sqrt(1.0 / (H * H + (d2 - d1) * (d2 - d1))) / 2.0
 
             # calculate errors
             e = dist_wanted - distance
-            e_d = (e - last_e) / dt
-            last_e = e
-            e_i += e * dt
-
-            # anti-wind up
-            e_i = e_i if abs(e_i) < 30 else math.copysign(1,e_i) * 30
 
             # calculate U
-            u_rot = K_p * e + K_d * e_d + K_i * e_i
+            u_rot = K_p * e
             if (abs(u_rot) > (100 - abs(fwd_V))):
                 u_rot = math.copysign(1,u_rot) * (100 - abs(fwd_V))
 
@@ -68,7 +56,7 @@ def main():
             mA.run_direct(duty_cycle_sp = u_left)
             mB.run_direct(duty_cycle_sp = u_right)
 
-            f.write(str(time.time() - start_time) + " " + str(distance) + " " + str(e_i))
+            f.write(str(time.time() - start_time) + " " + str(distance))
 
 
     finally:
